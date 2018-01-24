3c2c3c11c5e78a1baa27f0fbde8edeb4856872e3
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-01-24 14:00:41 -0500

    Added MessageBox once clicked

--- Lab02-ClickableNumbers/Form1.vb
+++ Lab02-ClickableNumbers/Form1.vb
@@ -1,21 +1,21 @@
 ﻿Public Class Form1
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-        Label1.Text = "One"
+        MessageBox.Show("One")
     End Sub
 
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
-        Label1.Text = "Two"
+        MessageBox.Show("Two")
     End Sub
 
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-        Label1.Text = "Three"
+        MessageBox.Show("Three")
     End Sub
 
     Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
-        Label1.Text = "Four"
+        MessageBox.Show("Four")
     End Sub
 
     Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
-        Label1.Text = "Five"
+        MessageBox.Show("Five")
     End Sub
 End Class
